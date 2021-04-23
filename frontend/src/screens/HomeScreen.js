d4cc175095ab35c7d1aff279ca60f347912db368
RazvanCelea96 <razvan.celea@vodafone.com>
2021-04-23 16:19:09 +0300

    d
    d

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  //cant use async await directly in useeffect, need a separate function
+  useEffect(() => {
+    const fetchProducts = async () => {
+      //if localhost:5000 added here we would get a cors error so we need a proxy in package.json on fe
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      //if localhost:5000 added here we would get a cors error so we need a proxy in package.json on fe
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
 
   return (
     <>
